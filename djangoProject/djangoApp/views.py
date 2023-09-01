051c06f926912ff68e647fa524d6eb7dda59a873
himanshu17-2000 <earthboxer@gmail.com>
2023-09-01 12:28:51 +0530

    Second commit

---
+++ djangoProject/djangoApp/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse ,request
+import json
+
+# Create your views here.
+def home_page(request):
+    return HttpResponse(json({'message':"Himanshu's home page"}))
